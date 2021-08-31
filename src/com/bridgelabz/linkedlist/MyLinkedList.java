179a05ae3f6bdf1bfaa5a507c24b4869f804c20b
Swathi <swathi.h@perfios.com>
2021-08-31 23:16:16 +0530

    ADDED: Method to push the node in ascending order to a linked list

--- src/com/bridgelabz/linkedlist/MyLinkedList.java
+++ src/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -96,43 +96,12 @@ public class MyLinkedList<K> {
 	}
 
 	public void orderedListAdd(INode<K> newNode) {
-		/*
-		 * if (this.tail == null) { this.tail = newNode; } if (this.head == null) {
-		 * this.head = newNode; } else { if ((int) this.head.getKey() > (int)
-		 * newNode.getKey()) { INode<K> nextNode = this.head; this.head = newNode;
-		 * newNode.setNext(nextNode); } else { INode<K> prevNode = this.head; INode<K>
-		 * tempNode = this.head.getNext(); boolean found = false;
-		 * System.out.println(prevNode.getKey()+ " " + tempNode.getKey()); while
-		 * (tempNode.getNext() != null && (int) tempNode.getKey() > (int)
-		 * newNode.getKey()) { prevNode = tempNode; tempNode = tempNode.getNext(); found
-		 * = true; } // if ((int) prevNode.getKey() == (int) this.head.getKey()) { //
-		 * head = newNode; // } else { // prevNode.setNext(newNode); // } //
-		 * newNode.setNext(tempNode); if (found) { prevNode.setNext(newNode);
-		 * prevNode.getNext().setNext(tempNode); } else {
-		 * 
-		 * } // tempNode.setNext(newNode); // if ((int) tempNode.getKey() < (int)
-		 * newNode.getKey()) { // } // INode<K> nextNode = tempNode.getNext(); //
-		 * tempNode = tempNode.getNext(); // tempNode.setNext(nextNode);
-		 * 
-		 * 
-		 * } }
-		 */
 
 		if (this.head == null || (int) head.getKey() > (int) newNode.getKey()) {
 			newNode.setNext(head);
 			head = newNode;
 		} else {
 			INode<K> currentNode = this.head;
-//			while(currentNode.getNext() != null && (int) currentNode.getKey() < (int) newNode.getKey()) {
-////				System.out.println("hi");
-////				currentNode = currentNode.getNext();
-////				System.out.println(currentNode.getNext().getKey());
-//				newNode.setNext(currentNode.getNext());
-//				System.out.println(newNode.getKey());
-//				currentNode.setNext(newNode);
-////				System.out.println(currentNode.getKey());
-//				break;
-//			}
 
 			boolean isInserted = false;
 			while (currentNode.getNext() != null) {
@@ -142,29 +111,13 @@ public class MyLinkedList<K> {
 					isInserted = true;
 					break;
 				} else {
-					System.out.println(
-							newNode.getKey() + " " + currentNode.getKey() + " " + currentNode.getNext().getKey());
-					currentNode.setNext(currentNode.getNext());
+					currentNode = currentNode.getNext();
 				}
 			}
+			if (!isInserted) {
+				currentNode.setNext(newNode);
+			}
 		}
-
-//		INode<K> currentNode = this.head;
-//		INode<K> tempNode;
-//		if(head == null) {
-//			return;
-//		}
-//		else {
-//			while(currentNode != null) {
-//				INode<K> nextNode = currentNode.getNext();
-//			
-//				while(nextNode != null) {
-//					if((int) currentNode.getKey() > (int) nextNode.getKey()) {
-//						
-//					}
-//			}
-//		}
-
 	}
 
 	public int size() {
