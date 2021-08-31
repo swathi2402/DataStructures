593cf1b69ef98b20191e17e42538c5a3d70ca8ba
Swathi <swathi.h@perfios.com>
2021-08-31 16:28:05 +0530

    ADDED: Input taking and creating linked list in main method

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++
@@ -1,5 +0,0 @@
-package com.bridgelabz.linkedlist;
-
-public class LinkedList {
-
-}
---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedListMain {
+	public static void main(String[] args) {
+		MyNode<Integer> firstNode = new MyNode<>(56);
+		MyNode<Integer> secondNode = new MyNode<>(30);
+		MyNode<Integer> thirdNode = new MyNode<>(70);
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+	}
+}
