b3f29ac06d5a2874744370db188cccea25e141a0
Swathi <swathi.h@perfios.com>
2021-08-31 16:12:13 +0530

    ADDED: Class Node is added with a key and next reference

---
+++ src/com/bridgelabz/linkedlist/MyNode.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.linkedlist;
+
+public class MyNode<K> {
+	private K key;
+	private MyNode next;
+
+	public MyNode(K key) {
+		super();
+		this.key = key;
+		this.next = null;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public MyNode getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+}
