72eedcb5222d29928ed1300486443d763ca70018
Swathi <swathi.h@perfios.com>
2021-08-31 22:11:57 +0530

    In intermediate step before finish

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -2,19 +2,15 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		MyNode<Integer> firstNode = new MyNode<>(56);
+		MyNode<Integer> firstNode = new MyNode<>(70);
 		MyNode<Integer> secondNode = new MyNode<>(30);
-		MyNode<Integer> thirdNode = new MyNode<>(70);
+		MyNode<Integer> thirdNode = new MyNode<>(40);
+		MyNode<Integer> fourthNode = new MyNode<>(60);
 		MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
-		myLinkedList.append(firstNode);
-		myLinkedList.append(thirdNode);
-		myLinkedList.insert(firstNode, secondNode);
-		MyNode<Integer> newNode = new MyNode<>(40);
-		myLinkedList.insertAtRandom(secondNode, newNode);
+		myLinkedList.orderedListAdd(firstNode);
+		myLinkedList.orderedListAdd(secondNode);
+		myLinkedList.orderedListAdd(thirdNode);
+		myLinkedList.orderedListAdd(fourthNode);
 		myLinkedList.printMyNodes();
-		myLinkedList.deleteAtRandom(newNode);
-		myLinkedList.printMyNodes();
-		int listSize = myLinkedList.size();
-		System.out.println("Size of the linked list is " + listSize);
 	}
 }
--- src/com/bridgelabz/linkedlist/MyLinkedList.java
+++ src/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -62,19 +62,19 @@ public class MyLinkedList<K> {
 		int count = -1;
 		INode<K> tempNode = this.head;
 		while (tempNode != null && tempNode.getNext() != null) {
-			count += 1 ;
-			if(tempNode.getKey().equals(key)) {
-				return count;			
+			count += 1;
+			if (tempNode.getKey().equals(key)) {
+				return count;
 			}
 			tempNode = tempNode.getNext();
 		}
 		return count;
 	}
-	
+
 	public void insertAtRandom(INode<K> keyNode, INode<K> newNode) {
 		INode<K> tempNode = this.head;
 		while (tempNode != null && tempNode.getNext() != null) {
-			if(tempNode.getKey().equals(keyNode.getKey())) {
+			if (tempNode.getKey().equals(keyNode.getKey())) {
 				INode<K> nextNode = tempNode.getNext();
 				tempNode.setNext(newNode);
 				tempNode = tempNode.getNext();
@@ -83,18 +83,90 @@ public class MyLinkedList<K> {
 			tempNode = tempNode.getNext();
 		}
 	}
-	
+
 	public void deleteAtRandom(INode<K> keyNode) {
 		INode<K> tempNode = this.head;
 		while (tempNode != null && tempNode.getNext() != null) {
-			if(!tempNode.getKey().equals(keyNode.getKey())) {
+			if (!tempNode.getKey().equals(keyNode.getKey())) {
 				tempNode = tempNode.getNext();
 			}
 			tempNode.setNext(tempNode.getNext().getNext());
 			break;
 		}
 	}
-	
+
+	public void orderedListAdd(INode<K> newNode) {
+		/*
+		 * if (this.tail == null) { this.tail = newNode; } if (this.head == null) {
+		 * this.head = newNode; } else { if ((int) this.head.getKey() > (int)
+		 * newNode.getKey()) { INode<K> nextNode = this.head; this.head = newNode;
+		 * newNode.setNext(nextNode); } else { INode<K> prevNode = this.head; INode<K>
+		 * tempNode = this.head.getNext(); boolean found = false;
+		 * System.out.println(prevNode.getKey()+ " " + tempNode.getKey()); while
+		 * (tempNode.getNext() != null && (int) tempNode.getKey() > (int)
+		 * newNode.getKey()) { prevNode = tempNode; tempNode = tempNode.getNext(); found
+		 * = true; } // if ((int) prevNode.getKey() == (int) this.head.getKey()) { //
+		 * head = newNode; // } else { // prevNode.setNext(newNode); // } //
+		 * newNode.setNext(tempNode); if (found) { prevNode.setNext(newNode);
+		 * prevNode.getNext().setNext(tempNode); } else {
+		 * 
+		 * } // tempNode.setNext(newNode); // if ((int) tempNode.getKey() < (int)
+		 * newNode.getKey()) { // } // INode<K> nextNode = tempNode.getNext(); //
+		 * tempNode = tempNode.getNext(); // tempNode.setNext(nextNode);
+		 * 
+		 * 
+		 * } }
+		 */
+
+		if (this.head == null || (int) head.getKey() > (int) newNode.getKey()) {
+			newNode.setNext(head);
+			head = newNode;
+		} else {
+			INode<K> currentNode = this.head;
+//			while(currentNode.getNext() != null && (int) currentNode.getKey() < (int) newNode.getKey()) {
+////				System.out.println("hi");
+////				currentNode = currentNode.getNext();
+////				System.out.println(currentNode.getNext().getKey());
+//				newNode.setNext(currentNode.getNext());
+//				System.out.println(newNode.getKey());
+//				currentNode.setNext(newNode);
+////				System.out.println(currentNode.getKey());
+//				break;
+//			}
+
+			boolean isInserted = false;
+			while (currentNode.getNext() != null) {
+				if ((int) currentNode.getNext().getKey() > (int) newNode.getKey()) {
+					newNode.setNext(currentNode.getNext());
+					currentNode.setNext(newNode);
+					isInserted = true;
+					break;
+				} else {
+					System.out.println(
+							newNode.getKey() + " " + currentNode.getKey() + " " + currentNode.getNext().getKey());
+					currentNode.setNext(currentNode.getNext());
+				}
+			}
+		}
+
+//		INode<K> currentNode = this.head;
+//		INode<K> tempNode;
+//		if(head == null) {
+//			return;
+//		}
+//		else {
+//			while(currentNode != null) {
+//				INode<K> nextNode = currentNode.getNext();
+//			
+//				while(nextNode != null) {
+//					if((int) currentNode.getKey() > (int) nextNode.getKey()) {
+//						
+//					}
+//			}
+//		}
+
+	}
+
 	public int size() {
 		int count = 0;
 		INode<K> tempNode = this.head;
